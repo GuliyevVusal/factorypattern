9d7c20f7cc1c01fb15e0d30c741c556d5e02547d
Vusal <143846119+GuliyevVusal@users.noreply.github.com>
2024-03-01 01:01:00 +0400

    Update Note9Fabriki.java

--- src/abstractfactory/Note9Fabriki.java
+++ src/abstractfactory/Note9Fabriki.java
@@ -2,7 +2,7 @@ package abstractfactory;
 
 public class Note9Fabriki implements TelefonFactory{
 
-    //    Factory Patternde biz bir fabrik qurmusduq telefon modeline gore eger S8 idise s8
+//    Factory Patternde biz bir fabrik qurmusduq telefon modeline gore eger S8 idise s8
 //    Note9 dursa Note9 istehsal et demisdik amma Fabrikde 100-lerle telefon modeli olsa
 //      if ("S8".equalsIgnoreCase(Model)) {
 //            telefon = new S8(Model, Battery, Eni, Uzunu);
@@ -11,7 +11,8 @@ public class Note9Fabriki implements TelefonFactory{
 //    yazmaq mumkun deyil Burda Abstract Factory gelir Abstract ne edir ?
 //    Telefon fabriki clasini da bolur S8 ucun ayri Fabrik Note9 ucun ayri fabrik yaradiriq
 //    diger Modeller olarsa onlarada ayri Fabrik yaradiriq class olaraq ve implemenet edirik
-//    TelefonFactoryni bu Bir interfacedir icerisinde Telefon tipinde getTelefon deye bir metod var ve Belelikle
+//    TelefonFactoryni bu Bir interfacedir icerisinde Telefon tipinde getTelefon deye bir metod var parametirleri
+//    (String model,String battery,int lenght, int hight)var ve Belelikle
 //    Magazalar S8 istedikde s8 fabrikinden Note9 istedikde Note9 fabrikinden Isteyirler
     @Override
     public Telefon getTelefon(String model, String battery, int length, int hight) {
