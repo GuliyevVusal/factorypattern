345e101120e1ca443f02f5c14663643c74f00280
Vusal <143846119+GuliyevVusal@users.noreply.github.com>
2024-02-29 23:39:19 +0400

    Update TelefonFabriki.java

--- src/factorypattern/TelefonFabriki.java
+++ src/factorypattern/TelefonFabriki.java
@@ -4,7 +4,7 @@ public class TelefonFabriki {
     // getTelefon metodunu miye Static olsun dedik ? Cunki Dillerler Telefon yaratmasin
     // TelefonFabriki uzerinden direkt olaraq static getTelefon metodunu Cagiracaq diller.Meqsedimiz
     // Obyekt yaratmaq ishini Fabrikaya vermekdirki Dillerle (wordTelecom) Telefon arasindaki Elaqeni
-    // Minumuma ENDIREK....
+    // Minumuma ENDIREK..Interfaceden istifade ederek musteriye yaratma mentiqini gostermeden obyekt yaradiriq
     // 100-lerle Telefon modeli olsa nece olacaq her model ucun else if else if yazsaq kodumuz uzancaq 
     // Bu qarisiqligin qarshisini almaq ucun Abstract Factory Pattern Yaradilmisdir
     public static Telefon getTelefon(String Model, String Battery, int hight, int length) {
